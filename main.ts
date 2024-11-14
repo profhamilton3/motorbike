1e06d2028f39fafbd0b308eda215efb97681eeb5
T. Hamilton <profhamilton3@gmail.com>
2024-11-13 20:47:32 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 input.onButtonPressed(Button.A, function () {
+    doPatternSwerve()
+})
+function doPatternSwerve () {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
     for (let index = 0; index <= 4; index++) {
         wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 90 + index * 5)
@@ -20,10 +23,29 @@ input.onButtonPressed(Button.A, function () {
         wuKong.setMotorSpeed(wuKong.MotorList.M1, -75 - index * -10)
     }
     wuKong.stopAllMotor()
-})
+}
 input.onButtonPressed(Button.B, function () {
-	
+    doPatternSwerve2()
 })
+function doPatternSwerve2 () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
+    for (let index = 0; index <= 4; index++) {
+        wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 90 + index * 5)
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, -20 + index * -10)
+        basic.pause(200)
+    }
+    basic.pause(1000)
+    for (let index = 0; index <= 5; index++) {
+        wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 105 - index * 5)
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, -30 + index * -10)
+        basic.pause(1000)
+    }
+    basic.pause(1000)
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 120)
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, -75)
+    basic.pause(5000)
+    wuKong.stopAllMotor()
+}
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     wuKong.stopAllMotor()
 })
