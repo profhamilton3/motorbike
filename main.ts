0b4645c57bb6afa4000aa262e1e646668fa7fafe
T. Hamilton <profhamilton3@gmail.com>
2024-11-13 21:31:44 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -29,18 +29,17 @@ input.onButtonPressed(Button.B, function () {
 function doPatternSwerve2 () {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
     for (let index = 0; index <= 4; index++) {
-        wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 90 + index * 5)
         wuKong.setMotorSpeed(wuKong.MotorList.M1, -20 + index * -10)
-        basic.pause(200)
-    }
-    basic.pause(1000)
-    for (let index = 0; index <= 5; index++) {
-        wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 105 - index * 5)
-        wuKong.setMotorSpeed(wuKong.MotorList.M1, -30 + index * -10)
         basic.pause(1000)
     }
     basic.pause(1000)
-    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 120)
+    for (let index = 0; index <= 3; index++) {
+        wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 105 - index * 5)
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, -40 + index * -10)
+        basic.pause(2000)
+    }
+    basic.pause(1000)
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 75)
     wuKong.setMotorSpeed(wuKong.MotorList.M1, -75)
     basic.pause(5000)
     wuKong.stopAllMotor()
